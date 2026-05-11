aaf8555a6c407269678014ce664bdead69809cfa
hsiey <hsiey@amazon.com>
2026-05-11 11:32:09 +0000

    add changelog page with version history in 3 languages

--- src/server.js
+++ src/server.js
@@ -34,6 +34,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/changelog', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/changelog.html'));
+});
+
 app.get('/dashboard', (req, res) => {
   if (!req.session.accessToken) {
     return res.redirect('/');
